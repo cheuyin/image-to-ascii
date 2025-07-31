35e70122018bd85aed9d8da4b693e46cb15efebe
cheuyin <yinstanleycheung@gmail.com>
2025-07-31 14:51:51 -0700

    Fix indexing issues with brightness_to_ascii helper when brightness=255

--- helpers.py
+++ helpers.py
@@ -1,17 +1,16 @@
 import math
 
-# Formula: Brightness = (R + G + B) / 3
-
 
 def convert_rgb_to_brightness(rgb_tuple: tuple[int, int, int]) -> int:
+    # Formula: Brightness = (R + G + B) / 3
     brightness = (rgb_tuple[0] + rgb_tuple[1] + rgb_tuple[2]) // 3
     return brightness
 
 
 def brightness_to_ascii(brightness: int) -> str:
     ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-    factor = 255 / len(ascii_chars)
-    idx = math.floor(brightness // factor)
+    factor = 256 / len(ascii_chars)
+    idx = math.floor(brightness / factor)
     return ascii_chars[idx]
 
 
